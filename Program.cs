048cb0799aedf45f078ca44196387085901b5583
kngval <bkvalteocs@gmail.com>
2025-01-16 20:36:18 +0800

    tcp

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Sockets;
+
+TcpListener myListener;
+int port = 5000; 
+
+System.Net.IPAddress local = System.Net.IPAddress.Parse();
+
+string webServerPath = @"WebServer";
+
+
+myListener = new TcpListener("127.0.0.1");
+
